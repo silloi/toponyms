41688561d3b4e244bcc62da526a23566bfed7499
silloi <silloi1993@gmail.com>
2021-08-01 13:08:04 +0900

    feature: create Header component

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="flex w-full px-5 h-16 items-center shadow fixed">
+      <p className="text-xl">怖い地名・難読地名</p>
+    </header>
+  )
+}
+
+export default Header
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,12 +1,10 @@
-import PREFECTURE from '../db/prefecture'
+import Header from './Header'
 import Footer from './Footer'
 
 function Layout({ children }) {
   return (
     <div className="flex flex-col h-screen">
-      <header className="flex w-full px-5 h-16 items-center shadow fixed">
-        <p className="text-xl">怖い地名・難読地名</p>
-      </header>
+      <Header />
       <div className="mt-16 overflow-y-auto">
         <main className="">{children}</main>
         <Footer />
