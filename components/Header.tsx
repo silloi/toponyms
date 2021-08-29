190927178666385ec17a136e11cebe0078fac24d
silloi <silloi1993@gmail.com>
2021-08-29 16:14:00 +0900

    feature: add top link on Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,12 @@
+import Link from 'next/link'
+
 const Header = () => {
   return (
     <header className="w-full shadow fixed">
       <div className="max-w-screen-md mx-auto flex px-5 h-16 items-center ">
-        <p className="text-xl">難読地名・怖い地名</p>
+        <Link href="/">
+          <a className="text-xl">難読地名・怖い地名</a>
+        </Link>
       </div>
     </header>
   )
