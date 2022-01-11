1010b68a0f5d7fa441881abb07fc03ae68082597
silloi <silloi1993@gmail.com>
2022-01-11 21:58:25 +0900

    fix: deal with build error

--- components/Map.tsx
+++ components/Map.tsx
@@ -9,8 +9,8 @@ const Map = ({ center, positionList }) => {
     <div>
       <Head>
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
-    integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
-    crossorigin=""/>
+          integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+          crossOrigin="" />
       </Head>
       <MapContainer center={center} zoom={zoom} style={{ height: "50vh" }}>
         <TileLayer
@@ -21,7 +21,7 @@ const Map = ({ center, positionList }) => {
           return (
             <Marker position={position}>
               <Tooltip permanent>
-                    <span>HOGE</span>
+                <span>HOGE</span>
               </Tooltip>
             </Marker>
           )
